4b9b1d87f4426f2e867f27da6fd3a7347c9b1079
unknown <mounikakondreddy67@gmail.com>
2025-04-04 21:10:31 +0530

    authCheck function Created

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -89,6 +89,7 @@ async function logout(req, res) {
 
 async function authCheck(req, res) {
     try {
+        console.log("req.user:", req.user);
         res.status(200).json({ success: true, user: req.user})
     } catch (error) {
         console.log('Error in authCheck controller:', error.message)
